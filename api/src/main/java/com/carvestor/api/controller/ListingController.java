dc3e16843a188cc42eba12bda3c68c341eabd0fb
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2024-07-02 22:09:26 +0530

    fixing check style errors

--- api/src/main/java/com/carvestor/api/controller/ListingController.java
+++ api/src/main/java/com/carvestor/api/controller/ListingController.java
@@ -2,7 +2,9 @@ package com.carvestor.api.controller;
 
 import com.carvestor.api.service.CarListingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
